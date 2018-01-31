69e7eb0facf1e0d3c683fc1bef81954946cc7d81
chuck.ql <chuck.ql@alibaba-inc.com>
2018-01-31 11:42:17 +0800

    feat: 增加引入时判断async支持机制

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 'use strict';
+function supportAsyncFunctions() {
+  try {
+    new Function('(async function () {})()');
+    return true;
+  } catch (ex) {
+    return false;
+  }
+}
+module.exports = supportAsyncFunctions() ? require('./src/client.js') : require('./lib/client.js');
 
-module.exports = require('./lib/client');
